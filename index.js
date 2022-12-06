518b34d3f2e9d07a077c9274d63bea6125fa023c
Jay P Hodges <jayphodges@gmail.com>
2022-12-05 18:31:21 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+    const bodyData = {
+        name: name,
+        email: email,
+    };
+    
+    const postMethod = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify(bodyData),
+    };
+    return fetch("http://localhost:3000/users", postMethod)
+        .then((response) => response.json())
+        .then((data) => {
+            
+            const body = document.querySelector('body');
+            const h2 = document.createElement('h2');
+            h2.innerHTML = data.id;
+            body.appendChild(h2);
+            
+        })
+
+        .catch((error) => {
+            console.log('There has been a problem with your fetch operation:', error);
+           
+            const body = document.querySelector('body');
+            const h2 = document.createElement('h2');
+            h2.innerHTML = error;
+            body.appendChild(h2);
+    });
+};
+
+submitData();
