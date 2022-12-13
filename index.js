a541a09efc912e30fb653d4a26c64afa14bcc714
Jay P Hodges <jayphodges@gmail.com>
2022-12-13 14:23:05 -0700

    commit tree 8b768c4ed8df94e62ec8d439a9549dc7c20d183e

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function submitData(name, email) {
         },
         body: JSON.stringify(bodyData),
     };
+
     return fetch("http://localhost:3000/users", postMethod)
         .then((response) => response.json())
         .then((data) => {
@@ -22,15 +23,12 @@ function submitData(name, email) {
             body.appendChild(h2);
             
         })
-
         .catch((error) => {
             console.log('There has been a problem with your fetch operation:', error);
-           
             const body = document.querySelector('body');
             const h2 = document.createElement('h2');
             h2.innerHTML = error;
             body.appendChild(h2);
     });
 };
-
 submitData();
